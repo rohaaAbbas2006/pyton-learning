c70d2313668c44a4893a9e3bc5b2a40fed60f7de
Rohaan Dev <rohaana505@gmail.com>
2025-06-17 14:45:03 +0500

    escape room tasks done

---
+++ classThree.py
@@ -0,0 +1,65 @@
+import random
+
+# Task One
+
+# userNum = int(input("Enter a number: "))
+# if userNum %2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# if userNum %3 == 0:
+#     print("multiple of 3")
+# else:
+#     print("not a multiple of 3")
+
+# if userNum > 100:
+#     print("greater than 100")
+# else:
+#     print("not greater than 100")
+
+# # Task Two
+
+# print("Hint: It's a classic phrase used in tales to open treasure caves.")
+# secret = input("Enter the secret phrase to open the door: ")
+
+# if secret.lower() == "open sesame":
+#     print("that a door opens.")
+# else:
+#     print("that a door does not open.")
+
+# Task Three
+
+# score = 0
+# for i in range(3):
+#     num = random.randint(1, 10)
+#     guess = int(input("Guess a number between 1 and 10: "))
+#     if guess == num:
+#         print("Correct!")
+#         score += 1
+#     else:
+#         print("Wrong! The number was", num)
+# print("Your total score is", score)
+
+# Task Four
+
+# a = input("Enter first value: ")
+# b = input("Enter second value: ")
+
+# if type(a) == type(b):
+#     print("Types match")
+# else:
+#     print("Types do not match")
+
+
+# Task Five
+
+# q1 = input("What is 5 + 3 * 2? ")
+# q2 = input("Is 10 > 20? (yes/no) ")
+# q3 = input("What's the type of 5.0? ")
+
+# if q1 == "11" and q2.lower() == "no" and q3.lower() == "<class 'float'>":
+#     print("You escaped the room!")
+# else:
+#     print("You're trapped forever.")
+
